7b8732f6533ca8338ee4d779c2d423f7e93ba1e3
Enzo Martins <menzo280@gmail.com>
2026-05-01 15:17:49 -0300

    Extend Idoso class from Usuario and implement methods

--- src/entities/Idoso.java
+++ src/entities/Idoso.java
@@ -1,5 +1,14 @@
 package entities;
 
-public class Idoso {
+public class Idoso extends Usuario {
 
-}
+		public Idoso(double saldo) {
+			super(saldo);
+		}
+
+		@Override
+		public void pagarPassagem() {
+			System.out.println("Idoso isento de tarifa.");
+			System.out.println("Saldo atual: R$ " + String.format("%.2f", saldo));
+		}
+	}
