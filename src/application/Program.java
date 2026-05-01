67c74359c6fd33b46a51bbc86b8333a3e141a02b
Enzo Martins <menzo280@gmail.com>
2026-05-01 15:16:49 -0300

    Implement main method with Usuario instances

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,25 @@
 package application;
+import entities.Comum;
+import entities.Estudante;
+import entities.Idoso;
+import entities.Usuario;
 
 public class Program {
 
-}
+	public static void main(String[] args) {
+
+			Usuario u1 = new Estudante(10.00);
+			Usuario u2 = new Idoso(3.00);
+			Usuario u3 = new Comum(8.00);
+
+			u1.pagarPassagem();
+			System.out.println("----------------------");
+
+			u2.pagarPassagem();
+			System.out.println("----------------------");
+
+			u3.pagarPassagem();
+		}
+	}
+
+
