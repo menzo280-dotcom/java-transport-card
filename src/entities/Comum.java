95cb56e38e5930a64c79ddce89a2ccc12abdf998
Enzo Martins <menzo280@gmail.com>
2026-05-01 15:17:15 -0300

    Extend Comum class from Usuario and implement methods

--- src/entities/Comum.java
+++ src/entities/Comum.java
@@ -1,5 +1,22 @@
 package entities;
 
-public class Comum {
+public class Comum extends Usuario {
 
-}
+		public Comum(double saldo) {
+			super(saldo);
+		}
+
+		@Override
+		public void pagarPassagem() {
+
+			if (saldo >= tarifa) {
+				saldo -= tarifa;
+
+				System.out.println("Passagem normal paga com sucesso!");
+				System.out.println("Saldo atual: R$ " + String.format("%.2f", saldo));
+
+			} else {
+				System.out.println("Saldo insuficiente.");
+			}
+		}
+	}
