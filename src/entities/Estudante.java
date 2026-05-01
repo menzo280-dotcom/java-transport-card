ffa5b6f3133603b8c5139499c6c484b57cb3089a
Enzo Martins <menzo280@gmail.com>
2026-05-01 15:17:33 -0300

    Extend Estudante class from Usuario and implement payment

--- src/entities/Estudante.java
+++ src/entities/Estudante.java
@@ -1,5 +1,24 @@
 package entities;
 
-public class Estudante {
+public class Estudante extends Usuario{
 
-}
+		public Estudante(double saldo) {
+			super(saldo);
+		}
+		@Override
+		public void pagarPassagem() {
+
+			double meiaTarifa = tarifa / 2;
+
+			if (saldo >= meiaTarifa) {
+				saldo -= meiaTarifa;
+
+				System.out.println("Passagem estudantil paga com sucesso!");
+				System.out.println("Saldo atual: R$ " + String.format("%.2f", saldo));
+
+			} else {
+				System.out.println("Saldo insuficiente.");
+			}
+		}
+	}
+
